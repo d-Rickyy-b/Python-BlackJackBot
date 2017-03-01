d9b1318ef6c271af9138f0ebd64f4a94067667c1
Rico <r.jambor@yahoo.de>
2017-03-01 22:43:06 +0100

    Add language modules and translation file

---
+++ lang/language.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import configparser
+
+__author__ = 'Rico'
+
+translations = configparser.ConfigParser()
+translations.read_file(codecs.open("lang/translations.ini", "r", "UTF-8"))
+
+
+# translation returns the translation for a specific string
+def translation(string, language):
+    if language in translations and string in translations[language]:
+        return translations[language][string]
+    elif language == "br":
+        return translations["pt_BR"][string]
+    elif "en" in translations and string in translations["en"]:
+        return translations["en"][string]
+    return string
