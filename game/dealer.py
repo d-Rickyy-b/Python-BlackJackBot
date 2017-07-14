8743cbac5e672afe9efa0f46dfae0aea35a6b045
Rico <r.jambor@yahoo.de>
2017-07-14 15:04:51 +0200

    FIX: wrong call of super method

--- game/dealer.py
+++ game/dealer.py
@@ -6,4 +6,4 @@ class Dealer(Player):
 
     def __init__(self, first_name, deck):
         user_id = -1
-        super().__init__(user_id, first_name, deck)
+        super(Dealer, self).__init__(user_id, first_name, deck)
