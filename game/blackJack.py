19095afed3ab70bb43276e318948ea48ed759c8a
Rico <r.jambor@yahoo.de>
2017-11-02 16:15:45 +0100

    TASK: Reformat code
    I also installed the save actions plugin to stop these commits from
    happening
    
    https://plugins.jetbrains.com/plugin/7642-save-actions

--- game/blackJack.py
+++ game/blackJack.py
@@ -158,20 +158,20 @@ class BlackJack(object):
     def start_game(self, message_id=None):
         if not self.game_running:
             if ((self.game_type == self.GROUP_CHAT or self.game_type == self.MULTIPLAYER_GAME) and len(self.players) > 1) or self.game_type == self.PRIVATE_CHAT:
-                    self.game_running = True
+                self.game_running = True
 
-                    for player in self.players:
-                        add_game_played(player.user_id)
+                for player in self.players:
+                    add_game_played(player.user_id)
 
-                    if self.game_type == self.GROUP_CHAT or self.game_type == self.MULTIPLAYER_GAME:
-                        self.send_message(self.chat_id, translate("gameBegins", self.lang_id) + "\n" + translate("gameBegins2", self.lang_id) + "\n\n" + self.get_player_overview(), game_id=self.__game_id)
+                if self.game_type == self.GROUP_CHAT or self.game_type == self.MULTIPLAYER_GAME:
+                    self.send_message(self.chat_id, translate("gameBegins", self.lang_id) + "\n" + translate("gameBegins2", self.lang_id) + "\n\n" + self.get_player_overview(), game_id=self.__game_id)
 
-                    self.dealers_turn()
-                    self.give_player_one()
+                self.dealers_turn()
+                self.give_player_one()
             else:
                 self.send_message(self.chat_id, translate("notEnoughPlayers", self.lang_id), message_id=message_id, game_id=self.__game_id)
         else:
-            #TODO Game already running
+            # TODO Game already running
             pass
 
     def evaluation(self):
