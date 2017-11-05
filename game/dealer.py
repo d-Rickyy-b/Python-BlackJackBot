3ff1b1af854fb8161170fd0bbc5a5e2435722a8a
Rico <r.jambor@yahoo.de>
2017-11-05 03:31:38 +0100

    FIX: Missing join_id argument for dealer

--- game/dealer.py
+++ game/dealer.py
@@ -6,6 +6,6 @@ from game.player import Player
 class Dealer(Player):
     credits = 0
 
-    def __init__(self, first_name, deck):
+    def __init__(self, first_name, deck, join_id=None):
         user_id = -1
-        super(Dealer, self).__init__(user_id, first_name, deck)
+        super(Dealer, self).__init__(user_id, first_name, deck, join_id)
