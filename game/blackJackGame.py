4772a9f93c219a703847b36e78bebb2a0c4d8564
Rico <r.jambor@yahoo.de>
2017-11-07 21:48:03 +0100

    TASK: Avoid unnecessary loop variable

--- game/blackJackGame.py
+++ game/blackJackGame.py
@@ -125,7 +125,7 @@ class BlackJackGame(object):
     def dealers_turn(self):
         if self.dealer.get_number_of_cards() < 2:
             card = None
-            for i in range(2):
+            for _ in range(2):
                 card = self.deck.pick_one_card()
                 self.dealer.give_card(card)
 
