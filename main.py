3acf41fedab445cf01d3b6e47ce537a42368f2d0
Rico <r.jambor@yahoo.de>
2018-03-08 11:30:27 +0100

    TASK: Reformat file

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ if not os.path.exists(os.path.join(logdir_path, "logs")):
 logfile_handler = logging.handlers.WatchedFileHandler(logfile_path, 'a', 'utf-8')
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, handlers=[logfile_handler])
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+                    handlers=[logfile_handler])
 
 if not re.match("[0-9]+:[a-zA-Z0-9\-_]+", BOT_TOKEN):
     logging.error("Bot token not correct - please check.")
@@ -140,7 +141,7 @@ def error(bot, update, error):
     """Log Errors caused by Updates."""
     if update is None:
         return
-    
+
     logger.warning('Update "%s" caused error "%s"', update, error)
 
     db = DBwrapper.get_instance()
@@ -164,7 +165,9 @@ def admin_method(func):
             return func(bot, update)
         else:
             update.message.reply_text('You have not the needed permissions to do that!')
-            logger.warning("User {} ({}, @{}) tried to use admin function '{}'!".format(user.id, user.first_name, user.username, func.__name__))
+            logger.warning(
+                "User {} ({}, @{}) tried to use admin function '{}'!".format(user.id, user.first_name, user.username,
+                                                                             func.__name__))
 
     return admin_check
 
