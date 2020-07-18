7fb3e35e8413565063e7977cd66e785ffec886d1
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-18 14:41:01 +0200

    refactor: rename deck class and internal deck card list

--- blackjack/game/deck.py
+++ blackjack/game/deck.py
@@ -8,26 +8,26 @@ from lang.language import translate
 __author__ = 'Rico'
 
 
-class CardDeck(object):
+class Deck(object):
 
     def __init__(self, lang_id="en"):
         self.lang_id = lang_id
         self.value_str = [translate("ace", lang_id), "2", "3", "4", "5", "6", "7", "8", "9", "10",
                           translate("jack", lang_id), translate("queen", lang_id), translate("king", lang_id)]
-        self.deck = []
+        self._deck = []
         self._set_up_deck()
         self._shuffle()
 
     def _set_up_deck(self):
-        self.deck = []
+        self._deck = []
 
         for card_id in range(52):
             card = Card(card_id, self.value_str)
-            self.deck.append(card)
+            self._deck.append(card)
 
     def _shuffle(self):
-        shuffle(self.deck)
+        shuffle(self._deck)
 
     def pick_one_card(self):
-        # TODO if len(self.deck) <= 0, then raise error
-        return self.deck.pop(0)
+        # TODO if len(self._deck) <= 0, then raise error
+        return self._deck.pop(0)
