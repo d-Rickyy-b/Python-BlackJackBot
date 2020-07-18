280f5eb4c4160a5d5ec0c7a70ab91881baf6fba0
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-18 14:38:28 +0200

    refactor: implement new cardvalue calculation
    Storing the cardvalue as a constant is not good. The values must be calculated dynamically

--- blackjack/game/player.py
+++ blackjack/game/player.py
@@ -12,27 +12,55 @@ class Player(object):
         self.first_name = first_name
         self.join_id = join_id
         self.lang_id = lang_id
-        self.cardvalue = 0
         self.has_ace = False
         self.cards = []
 
     def give_card(self, card: Card):
         self.cards.append(card)
 
-        if card.value == 11 and self.cardvalue <= 10:
-            self.give_ace()
-        elif card.value == 11 and (self.cardvalue + 11) > 21:
-            self.cardvalue += 1
-            return
+    @property
+    def cardvalue(self):
+        """
+        Calculate the current value of the cards on the hand
+        :return: Current value of the cards on the hand
+        """
+        value = 0
+        ace_counter = 0
 
-        self.cardvalue += card.value
+        for card in self.cards:
+            if card.is_ace():
+                # if we encounter an ace, we don't calculate it yet
+                ace_counter += 1
+            else:
+                # If no ace we simply add the value to the total
+                value += card.value
 
-    def give_ace(self):
-        self.has_ace = True
+        if ace_counter == 0:
+            return value
 
-    def remove_ace(self):
-        self.has_ace = False
-        self.cardvalue -= 10
+        possible_values = set()
+
+        # We now calculate all the possible values combinations of the cards + aces
+        for i in range(ace_counter + 1):
+            possible_values.add(value + (i * 11) + ((ace_counter - i) * 1))
+
+        possible_values_list = list(possible_values)
+        # Sort the list so that we can later on work with returning the first/last element of a list without checking actual values again
+        possible_values_list.sort()
+
+        # Split up the results into two list for easier handling
+        lower_21 = [i for i in possible_values_list if i <= 21]
+        bigger_21 = [i for i in possible_values_list if i > 21]
+
+        # If we have an ace, we want to return the biggest value below 21 (is possible).
+        if len(lower_21) >= 1:
+            return lower_21[-1]
+
+        # If there is no such value we want to return the lowest value above 21
+        if len(bigger_21) >= 1:
+            return bigger_21[0]
+
+        raise ValueError
 
     def has_cards(self):
         return len(self.cards) > 0
