7826a9f510cf0d20ab2e5bc9fe8110c819c2fc71
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-20 00:57:09 +0200

    test: add tests for the card class

--- blackjack/game/tests/card_test.py
+++ blackjack/game/tests/card_test.py
@@ -17,16 +17,116 @@ class CardTest(unittest.TestCase):
         Check that the card is properly created
         :return:
         """
-        card = self._generate_card(1)
+        card = self._generate_card(0)
+        self.assertEqual(11, card.value)
+        self.assertEqual(0, card.card_id)
 
-        self.assertEqual(1, card.value)
+        card = self._generate_card(1)
+        self.assertEqual(2, card.value)
+        self.assertEqual(1, card.card_id)
+
+        card = self._generate_card(2)
+        self.assertEqual(3, card.value)
+        self.assertEqual(2, card.card_id)
+
+        card = self._generate_card(3)
+        self.assertEqual(4, card.value)
+        self.assertEqual(3, card.card_id)
+
+        card = self._generate_card(4)
+        self.assertEqual(5, card.value)
+        self.assertEqual(4, card.card_id)
+
+        card = self._generate_card(5)
+        self.assertEqual(6, card.value)
+        self.assertEqual(5, card.card_id)
+
+        card = self._generate_card(6)
+        self.assertEqual(7, card.value)
+        self.assertEqual(6, card.card_id)
+
+        card = self._generate_card(7)
+        self.assertEqual(8, card.value)
+        self.assertEqual(7, card.card_id)
+
+        card = self._generate_card(8)
+        self.assertEqual(9, card.value)
+        self.assertEqual(8, card.card_id)
+
+        card = self._generate_card(9)
+        self.assertEqual(10, card.value)
+        self.assertEqual(9, card.card_id)
+
+        card = self._generate_card(10)
+        self.assertEqual(10, card.value)
+        self.assertEqual(10, card.card_id)
+
+        card = self._generate_card(11)
+        self.assertEqual(10, card.value)
+        self.assertEqual(11, card.card_id)
+
+        card = self._generate_card(12)
+        self.assertEqual(10, card.value)
+        self.assertEqual(12, card.card_id)
+
+        card = self._generate_card(13)
+        self.assertEqual(11, card.value)
+        self.assertEqual(13, card.card_id)
+
+        card = self._generate_card(14)
+        self.assertEqual(2, card.value)
+        self.assertEqual(14, card.card_id)
 
     def test_is_ace(self):
         """
-        Check that shuffeling actually changes order of cards
+        Check if cards return correct values when they are aces
         :return:
         """
-        pass
+        card = self._generate_card(0)
+        self.assertEqual(11, card.value)
+        self.assertTrue(card.is_ace())
+
+        card = self._generate_card(1)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(2)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(3)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(4)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(5)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(6)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(7)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(8)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(9)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(10)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(11)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(12)
+        self.assertFalse(card.is_ace())
+
+        card = self._generate_card(13)
+        self.assertTrue(card.is_ace())
+
+        card = self._generate_card(14)
+        self.assertFalse(card.is_ace())
 
 
 if __name__ == '__main__':
