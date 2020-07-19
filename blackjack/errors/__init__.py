4e2569e43b79ab1a6ed5bd670fb34c888cd3131d
Rico <d-Rickyy-b@users.noreply.github.com>
2020-07-19 15:41:30 +0200

    feat: implement exceptions for handling error states

--- blackjack/errors/__init__.py
+++ blackjack/errors/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .playerbustedexception import PlayerBustedException
+from .gamealreadyrunningexception import GameAlreadyRunningException
+from .playeralreadyexistingexception import PlayerAlreadyExistingException
+from .maxplayersreachedexception import MaxPlayersReachedException
 
-__all__ = ['PlayerBustedException']
+__all__ = ['PlayerBustedException', 'GameAlreadyRunningException', 'PlayerAlreadyExistingException', 'MaxPlayersReachedException']
