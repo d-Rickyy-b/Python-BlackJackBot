6e935e7ca667e5f0b02729fc12d4e6e45636c345
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-17 00:24:52 +0200

    reformat: move bot setup code
    Move the setup code to the bot package init file. Also import the

--- blackjackbot/__init__.py
+++ blackjackbot/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+
+from .bot import handlers
+
+__all__ = ['handlers']
--- blackjackbot/bot/__init__.py
+++ blackjackbot/bot/__init__.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+from telegram.ext import CommandHandler, CallbackQueryHandler
+
+from blackjackbot.bot.commands import start_cmd, stop_cmd, stats_cmd, language_cmd, hit_callback, stand_callback, join_callback, start_callback, \
+    newgame_callback
+
+start_command_handler = CommandHandler("start", start_cmd)
+stop_command_handler = CommandHandler("stop", stop_cmd)
+language_command_handler = CommandHandler("language", language_cmd)
+stats_command_handler = CommandHandler("stats", stats_cmd)
+
+hit_callback_handler = CallbackQueryHandler(hit_callback, pattern=r"^hit$")
+stand_callback_handler = CallbackQueryHandler(stand_callback, pattern=r"^stand$")
+join_callback_handler = CallbackQueryHandler(join_callback, pattern=r"^join$")
+start_callback_handler = CallbackQueryHandler(start_callback, pattern=r"^start$")
+newgame_callback_handler = CallbackQueryHandler(newgame_callback, pattern=r"^newgame$")
+
+handlers = [start_command_handler, stop_command_handler, join_callback_handler, hit_callback_handler, stand_callback_handler, start_callback_handler,
+            language_command_handler, stats_command_handler, newgame_callback_handler]
+
+
+__all__ = ['handlers']
