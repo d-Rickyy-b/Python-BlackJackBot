43edbf816eaaf62f8d7fae08ebc1ebebecc70721
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-23 04:47:25 +0200

    fix: missing import

--- blackjackbot/bot/commands/__init__.py
+++ blackjackbot/bot/commands/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .game.commands import start_cmd, stop_cmd, hit_callback, stand_callback, join_callback, start_callback, newgame_callback
+from .game.commands import start_cmd, stop_cmd, hit_callback, stand_callback, join_callback, start_callback, newgame_callback, rules_cmd
 from .settings.commands import language_cmd, language_callback
 
 __all__ = ['start_cmd', 'stop_cmd', 'stats_cmd', 'language_cmd', 'rules_cmd', 'hit_callback', 'stand_callback', 'join_callback', 'start_callback',
