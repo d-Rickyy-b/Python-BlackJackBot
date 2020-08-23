d19124156ef37c383f2bde5f1d0007b217dddab2
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-23 04:49:20 +0200

    feat: implement command to reload the i8n strings

--- blackjackbot/bot/__init__.py
+++ blackjackbot/bot/__init__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from telegram.ext import CommandHandler, CallbackQueryHandler
 
-from blackjackbot.bot.commands import start_cmd, stop_cmd, stats_cmd, language_cmd, hit_callback, stand_callback, join_callback, start_callback, \
-    newgame_callback
+from blackjackbot.bot.commands import start_cmd, stop_cmd, stats_cmd, language_cmd, \
+    hit_callback, stand_callback, join_callback, start_callback, newgame_callback, language_callback, \
+    reload_languages_cmd
 
 start_command_handler = CommandHandler("start", start_cmd)
 stop_command_handler = CommandHandler("stop", stop_cmd)
 language_command_handler = CommandHandler("language", language_cmd)
 stats_command_handler = CommandHandler("stats", stats_cmd)
+reload_lang_command_handler = CommandHandler("reload_lang", reload_languages_cmd)
 
 hit_callback_handler = CallbackQueryHandler(hit_callback, pattern=r"^hit$")
 stand_callback_handler = CallbackQueryHandler(stand_callback, pattern=r"^stand$")
@@ -16,7 +18,6 @@ start_callback_handler = CallbackQueryHandler(start_callback, pattern=r"^start$"
 newgame_callback_handler = CallbackQueryHandler(newgame_callback, pattern=r"^newgame$")
 
 handlers = [start_command_handler, stop_command_handler, join_callback_handler, hit_callback_handler, stand_callback_handler, start_callback_handler,
-            language_command_handler, stats_command_handler, newgame_callback_handler]
-
+            language_command_handler, stats_command_handler, newgame_callback_handler, reload_lang_command_handler]
 
 __all__ = ['handlers']
