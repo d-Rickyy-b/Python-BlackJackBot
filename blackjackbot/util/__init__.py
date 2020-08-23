e2acffe3ac8a64aeea85fb9bf5c374e29844231a
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-23 03:36:13 +0200

    feat: implement method to build multi col inline telegram menus

--- blackjackbot/util/__init__.py
+++ blackjackbot/util/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .textutils import html_mention, get_game_keyboard, get_join_keyboard, generate_evaluation_string, get_start_keyboard
+from .textutils import html_mention, get_game_keyboard, get_join_keyboard, generate_evaluation_string, get_start_keyboard, build_menu
 
-__all__ = ['html_mention', 'get_game_keyboard', 'get_join_keyboard', 'generate_evaluation_string', 'get_start_keyboard']
+__all__ = ['html_mention', 'get_game_keyboard', 'get_join_keyboard', 'generate_evaluation_string', 'get_start_keyboard', 'build_menu']
--- blackjackbot/util/textutils.py
+++ blackjackbot/util/textutils.py
@@ -47,3 +47,12 @@ def get_join_keyboard():
 def get_start_keyboard():
     start_button = InlineKeyboardButton(text="Start", callback_data="start")
     return InlineKeyboardMarkup(inline_keyboard=[[start_button]])
+
+
+def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
