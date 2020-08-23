b0e36a622da1f578d6876d59a14f828756de1e87
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-24 01:16:39 +0200

    refactor: remove unused code

--- blackjackbot/util/__init__.py
+++ blackjackbot/util/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .textutils import html_mention, get_game_keyboard, get_join_keyboard, generate_evaluation_string, get_start_keyboard, build_menu
+from .textutils import build_menu
 from .misc import get_cards_string
 
-__all__ = ['html_mention', 'get_game_keyboard', 'get_join_keyboard', 'generate_evaluation_string', 'get_start_keyboard', 'build_menu',
-           'get_cards_string']
+__all__ = ['build_menu', 'get_cards_string']
