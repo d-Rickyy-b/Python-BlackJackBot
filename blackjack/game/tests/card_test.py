0425d33195c43112b3e58eaf6f99302d61ee061d
Rico <d-Rickyy-b@users.noreply.github.com>
2020-08-23 03:34:19 +0200

    test: generalize testing of cards
    The previous test was hard coded. This new way of testing allows for less lines of code at higher coverage

--- blackjack/game/tests/card_test.py
+++ blackjack/game/tests/card_test.py
@@ -8,8 +8,7 @@ class CardTest(unittest.TestCase):
 
     @staticmethod
     def _generate_card(value):
-        val_str = []
-        card = Card(value, val_str)
+        card = Card(value)
         return card
 
     def test_creation(self):
@@ -17,116 +16,36 @@ class CardTest(unittest.TestCase):
         Check that the card is properly created
         :return:
         """
-        card = self._generate_card(0)
-        self.assertEqual(11, card.value)
-        self.assertEqual(0, card.card_id)
+        for multiplier in range(4):
+            offset = multiplier * 13
+            # Numbers
+            for i in range(offset, offset + 9):
+                card = self._generate_card(i)
+                self.assertEqual(i + 2 - offset, card.value)
+                self.assertEqual(i, card.card_id)
 
-        card = self._generate_card(1)
-        self.assertEqual(2, card.value)
-        self.assertEqual(1, card.card_id)
+            # Jack, Queen, King
+            for i in range(offset + 9, offset + 12):
+                card = self._generate_card(i)
+                self.assertEqual(10, card.value)
+                self.assertEqual(i, card.card_id)
 
-        card = self._generate_card(2)
-        self.assertEqual(3, card.value)
-        self.assertEqual(2, card.card_id)
-
-        card = self._generate_card(3)
-        self.assertEqual(4, card.value)
-        self.assertEqual(3, card.card_id)
-
-        card = self._generate_card(4)
-        self.assertEqual(5, card.value)
-        self.assertEqual(4, card.card_id)
-
-        card = self._generate_card(5)
-        self.assertEqual(6, card.value)
-        self.assertEqual(5, card.card_id)
-
-        card = self._generate_card(6)
-        self.assertEqual(7, card.value)
-        self.assertEqual(6, card.card_id)
-
-        card = self._generate_card(7)
-        self.assertEqual(8, card.value)
-        self.assertEqual(7, card.card_id)
-
-        card = self._generate_card(8)
-        self.assertEqual(9, card.value)
-        self.assertEqual(8, card.card_id)
-
-        card = self._generate_card(9)
-        self.assertEqual(10, card.value)
-        self.assertEqual(9, card.card_id)
-
-        card = self._generate_card(10)
-        self.assertEqual(10, card.value)
-        self.assertEqual(10, card.card_id)
-
-        card = self._generate_card(11)
-        self.assertEqual(10, card.value)
-        self.assertEqual(11, card.card_id)
-
-        card = self._generate_card(12)
-        self.assertEqual(10, card.value)
-        self.assertEqual(12, card.card_id)
-
-        card = self._generate_card(13)
-        self.assertEqual(11, card.value)
-        self.assertEqual(13, card.card_id)
-
-        card = self._generate_card(14)
-        self.assertEqual(2, card.value)
-        self.assertEqual(14, card.card_id)
+            # Ace
+            card = self._generate_card(offset + 12)
+            self.assertEqual(11, card.value)
+            self.assertEqual(offset + 12, card.card_id)
 
     def test_is_ace(self):
         """
         Check if cards return correct values when they are aces
         :return:
         """
-        card = self._generate_card(0)
-        self.assertEqual(11, card.value)
-        self.assertTrue(card.is_ace())
-
-        card = self._generate_card(1)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(2)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(3)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(4)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(5)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(6)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(7)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(8)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(9)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(10)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(11)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(12)
-        self.assertFalse(card.is_ace())
-
-        card = self._generate_card(13)
-        self.assertTrue(card.is_ace())
-
-        card = self._generate_card(14)
-        self.assertFalse(card.is_ace())
+        for i in range(52):
+            card = self._generate_card(i)
+            if card.value == 11:
+                self.assertTrue(card.is_ace())
+            else:
+                self.assertFalse(card.is_ace())
 
 
 if __name__ == '__main__':
