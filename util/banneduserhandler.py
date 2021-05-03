07c6a390754397967bd30277cb24196b9d356907
Rico <d-Rickyy-b@users.noreply.github.com>
2021-05-03 20:15:49 +0200

    fix: check if user object is None

--- util/banneduserhandler.py
+++ util/banneduserhandler.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from telegram.ext import TypeHandler
 
 import database
 
 
 class BannedUserHandler(TypeHandler):
+    logger = logging.getLogger()
 
     def check_update(self, update):
         db = database.Database()
-        if db.is_user_banned(update.effective_user.id):
+        user = update.effective_user
+
+        if user is None:
+            self.logger.warning(f"User is None! Update: {update}")
+            return False
+
+        if db.is_user_banned(user.id):
             return True
+
         return False
